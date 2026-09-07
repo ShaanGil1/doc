4f5c93d6786def7a8b26b85ce4f324bd55d11215
Shaan Gill <71356331+ShaanGil1@users.noreply.github.com>
2026-09-07 15:34:57 -0400

    Delete scripts directory

--- scripts/up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-docker compose up -d
-echo
-echo "Doccano should be at: http://localhost:8000"
-echo "Login:"
-echo "  username: admin"
-echo "  password: password"
